26d03d6051ad5c6ffcb00bd8bbb9e5887e967fbc
Sara Ward <saraward@MacBook-Pro-6.local>
2020-11-24 16:32:15 -0500

    content on and connected

--- src/pages/dev.js
+++ src/pages/dev.js
@@ -5,11 +5,19 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const DevPage = props => {
+  const devBio =
+    props.data.allContentfulSoftwareImagesDevBioTextNode.nodes[0].devBio
+
   console.log(props)
   return (
     <Layout>
       <SEO title="Graphic design" />
-
+      <h1>
+        <center>Full Stack Development</center>
+      </h1>
+      {devBio}
+      <br />
+      <br />
       {/* <Link to="/">Go back to the homepage</Link> */}
 
       {props.data.allContentfulSoftwareImages.edges[0].node.softwareDevImages.map(
@@ -30,9 +38,13 @@ export const devPageQuery = graphql`
             fluid(maxWidth: 900, quality: 100) {
               ...GatsbyContentfulFluid
             }
-            id
           }
         }
+      }
+    }
+    allContentfulSoftwareImagesDevBioTextNode {
+      nodes {
+        devBio
       }
     }
   }
