4300eb866a4628500590653116d41fe2aded292b
Sara Ward <saraward@MacBook-Pro-6.local>
2020-11-24 09:19:03 -0500

    error window

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,47 +5,17 @@
  */
 
 // You can delete this file if you're not using it
-// const path = require(`path`)
-// const { pseudoRandomBytes } = require("crypto")
-// // const { createFilePath } = require(`gatsby-source-filesystem`)
-
-// exports.createPages = ({ graphql, actions }) => {
-//   const { createPage } = actions
-
-//   const imagePage = path.resolve(`./src/templates/image-page-contentful`)
-//   return graphql(
-//     `
-//       {
-//         allContentfulGraphicImage {
-//           edges {
-//             node {
-//               graphicDesignImages {
-//                 id
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `
-//   ).then(result => {
-//     if (result.errors) {
-//       throw result.errors
-//     }
-//     //create image posts
-//     const images = result.data.allContentfulGraphicImage.edges
-
-//     images.forEach((image, index) => {
-//       const previous = index === images ? null : images[index + 1].node
-//       const next = index === 0 ? null : images[index - 1].node
-//     })
-//     createPage({
-//       path: post.node.graphicDesignImage,
-//       component: imagePage,
-//       context: {
-//         id: post.node.id,
-//         previous,
-//         next,
-//       },
-//     })
-//   })
-// }
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bad-module/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
