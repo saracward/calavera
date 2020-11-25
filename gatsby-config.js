1f2cd59fc8d006e5131cdc9f9d74f1047e8be577
Sara Ward <saraward@MacBook-Pro-6.local>
2020-11-24 20:42:51 -0500

    final steps

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ if (process.env.NODE_ENV !== "production") {
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Calavera`,
+    description: ``,
     author: `@gatsbyjs`,
   },
   plugins: [
